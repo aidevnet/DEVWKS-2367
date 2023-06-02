f81b7796a8f706c53487a9dbd2495fd9b8dc4504
aidevnet <ailiesiu@cisco.com>
2023-06-01 21:20:57 -0700

    Update __main__.py
    Updated port to 8670

--- SD-WAN-MSP-Demo-Open-Back-End/swagger_server/__main__.py
+++ SD-WAN-MSP-Demo-Open-Back-End/swagger_server/__main__.py
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Self Service Demo: Operations Application'})
-    app.run(port=8080)
+    app.run(port=8670)
 
 
 if __name__ == '__main__':
